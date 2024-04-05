75670633607b9a7bbcd5cad99d450a35612d24b9
Marc-Andre Lord <mandrelord@gmail.com>
2024-04-05 14:48:34 -0400

    updated auto_config.sh

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -5,8 +5,35 @@ echo "SLEEP START"
 sleep 10
 echo "SLEEP END"
 
-wp config create --allow-root \
-    --dbname=$SQL_DATABASE \
-    --dbuser=$SQL_USER \
-    --dbpass=$SQL_PASSWORD \
-    --dbhost=mariadb:3306 --path='/var/www/wordpress'
+if [ -f "$wp_config_file" ]; then
+	echo "Deleting wp-config.php"
+	rm -rf $wp_config_file
+fi
+
+if [ ! -f "$wp_config_file" ]; then
+	echo "setuping php..."
+	wp config create --allow-root \
+    	--dbname=$SQL_DATABASE \
+    	--dbuser=$SQL_USER \
+    	--dbpass=$SQL_PASSWORD \
+    	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+	echo "Creating page..."
+	/usr/local/bin/wp-cli.phar --allow-root core install \
+	--url='https://localhost' \
+	--title="$WP_TITLE" \
+	--admin_user="$WP_ADMIN" \
+	--admin_password="$WP_ADMIN_PASSWORD"
+	--admin_email="$WP_ADMIN_EMAIL" \
+	--path='var/www/wordpress/'
+	echo "Creating new user..."
+	/usr/local/bin/wp-cli.phar --allow-root user create \
+	"$WP_USER" \
+	"$WP_USER_EMAIL" \
+	--role=author \
+	--user_pass="$WP_USER_PASSWORD" \
+	--path='/var/www/wordpress/'
+	echo "To check second user creation, go to localhost/wp-login.php"
+fi
+
+echo "Executing php-fpm"
+/usr/sbin/php-fpm7.3 -F
