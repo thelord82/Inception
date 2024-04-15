7d426dfe9de68c9c5f95a2ca3d1f4b63cf0198fa
Marc-Andre Lord <mandrelord@gmail.com>
2024-04-15 18:21:53 -0400

    Fixings in progress...where is wp ?

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -21,7 +21,7 @@ if [ ! -f "$wp_config_file" ]; then
 	--url='https://localhost' \
 	--title="$WP_TITLE" \
 	--admin_user="$WP_ADMIN" \
-	--admin_password="$WP_ADMIN_PASSWORD"
+	--admin_password="$WP_ADMIN_PASSWORD" \
 	--admin_email="$WP_ADMIN_EMAIL" \
 	--path='var/www/wordpress/'
 	echo "Creating new user..."
